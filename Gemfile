6a8a476ab78ad5b2f789775af80a862a1e40360a
Sameet Sapra <sameetandpotatoes@gmail.com>
2017-08-21 19:39:31 -0500

    Add Travis CI

--- Gemfile
+++ Gemfile
@@ -1,16 +1,16 @@
 # Copyright © 2017, ACM@UIUC
 #
-# This file is part of the Groot Project.  
-# 
+# This file is part of the Groot Project.
+#
 # The Groot Project is open source software, released under the University of
 # Illinois/NCSA Open Source License. You should have received a copy of
 # this license in a file with the distribution.
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 gem 'bundler'
 gem 'rake'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
-gem "sinatra-cross_origin", "~> 0.3.1"
+gem 'sinatra-cross_origin', '~> 0.3.1'
--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 # Copyright © 2017, ACM@UIUC
 #
-# This file is part of the Groot Project.  
-# 
+# This file is part of the Groot Project.
+#
 # The Groot Project is open source software, released under the University of
 # Illinois/NCSA Open Source License. You should have received a copy of
 # this license in a file with the distribution.
@@ -11,58 +11,58 @@ require 'json'
 
 set :port, 8008
 
-fake_token = "this-is-a-fake-token"
+fake_token = 'this-is-a-fake-token'
 fake_user = {
-    "first-name": "Mr. Fake",
-    "last-name": "User",
-    "name": "jsmith2",
-    "email": "jsmith2@illinois.edu"
+  'first-name' => 'Fake',
+  'last-name' => 'User',
+  'name' => 'jsmith2',
+  'email' => 'jsmith2@illinois.edu'
 }
 
 post '/session' do
   {
-    "token": fake_token,
-    "user": {
-      "link": {
-        "href": "#{request.base_url}/user?username=#{fake_user[:name]}",
-        "rel": "self"
+    'token' => fake_token,
+    'user' => {
+      'link' => {
+        'href' => "#{request.base_url}/user?username=#{fake_user[:name]}",
+        'rel' => 'self'
       },
-      "name": fake_user[:name]
+      'name' => fake_user[:name]
     },
-    "link": {
-      "href": "#{request.base_url}/session/#{fake_token}",
-      "rel": "self"
+    'link' => {
+      'href' => "#{request.base_url}/session/#{fake_token}",
+      'rel' => 'self'
     }
   }.to_json
 end
 
 delete '/session' do
   {
-    "message": "OK"
+    'message' => 'OK'
   }.to_json
 end
 
 get '/session/:token' do
-  halt 500, {"error" => "Invalid token provided"}.to_json unless params[:token] == fake_token
+  halt 500, { 'error' => 'Invalid token provided' }.to_json unless params[:token] == fake_token
 
   {
-    "token": fake_token,
-    "user": fake_user,
-    "link": {
-      "href": "#{request.base_url}/session/#{fake_token}",
-      "rel": "self"
+    'token' => fake_token,
+    'user' => fake_user,
+    'link' => {
+      'href' => "#{request.base_url}/session/#{fake_token}",
+      'rel' => 'self'
     }
   }.to_json
 end
 
 post '/session/:token' do
   {
-    "message": "OK"
+    'message' => 'OK'
   }.to_json
 end
 
 delete '/session/:token' do
   {
-    "message": "OK"
+    'message' => 'OK'
   }.to_json
 end
